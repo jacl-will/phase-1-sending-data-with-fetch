3f37b1efc2cd3f9fea87cbc148ca9fa8a82df0ab
Jaclyn Williams <jaclyn.williams143@gmail.com>
2023-01-16 21:37:31 -0500

    commit tree 1cfdb9276c47434c0d5c21240f5d9af6d9b167f3

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+// const newUser = {
+//     name: `${userName}`,
+//     email: `${userEmail}`,
+// }
+
+function submitData(name, email){
+    const newUser = {
+        name,
+        email,
+        // name: `${userName}`,
+        // email: `${userEmail}`,
+    };
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(newUser),
+};
+
+return fetch("http://localhost:3000/users", configurationObject)
+// .then(resp => resp.json())
+// .then(newUser => console.log(newUser))
+.then(function(resp) {
+    return resp.json();
+})
+.then(function(obj) {
+    document.body.innerHTML = object('id');
+})
+.catch(function(error) {
+    document.body.innerHTML = error.message;
+});
+}
